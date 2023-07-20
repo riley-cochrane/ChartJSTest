87dbc7b666f1fb333c82e75c97490c36faffed3b
Riley Cochrane <Riley.Cochrane@enlighten.co.nz>
2023-07-20 17:13:50 +1200

    Add project files.

---
+++ DataItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ChartJSTest
+{
+
+	public class DataItem
+	{
+		/// <summary>
+		/// Gets or sets the group.
+		/// </summary>
+		/// <value>
+		/// The group.
+		/// </value>
+		[JsonPropertyName("group")]
+		public string? Group { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name of the attribute
+		/// </summary>
+		/// <value>
+		/// The name of the attribute
+		/// </value>
+		[JsonPropertyName("name")]
+		public string? Name { get; set; }
+
+		/// <summary>
+		/// Gets or sets the value.
+		/// </summary>
+		/// <value>
+		/// The value
+		/// </value>
+		[JsonPropertyName("value")]
+		public decimal? Value { get; set; }
+	}
+}
